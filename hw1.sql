34890f2ad5e4f22bb02d2dcec8da89f4ba40e297
Ünal <51839596+ucarmustafaunal@users.noreply.github.com>
2024-02-02 14:45:45 +0300

    Add files via upload
    Comparison Operators work 1

---
+++ hw1.sql
@@ -0,0 +1,13 @@
+select f.title, f.description from film f;
+
+select * from film where length > 60 and length < 75 order by length desc;
+
+select * from film where rental_rate = 0.99 
+and (replacement_cost = 12.99 or replacement_cost = 28.99);
+
+select last_name from customer where first_name = 'Mary';
+
+select * from film where 
+not length > 50 
+and 
+not (rental_rate = 2.99 or rental_rate = 4.99);
